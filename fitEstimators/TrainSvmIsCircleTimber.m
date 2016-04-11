4325654f71589eef63267b3335de7f8973e66261
Vladimir1994 <vladimir.matveev.1994@mail.com>
2016-04-12 01:07:48 +0300

    .

--- fitEstimators/TrainSvmIsCircleTimber.m
+++
@@ -1,6 +0,0 @@
-function [svmModel, kFoldLoss] = trainSvmIsCircleTimber(features, labels)
-    svmModel = fitcsvm(features, labels, 'Standardize', true);
-    cvSvmModel = crossval(svmModel);
-    kFoldLoss = kfoldLoss(cvSvmModel);
-end
-
